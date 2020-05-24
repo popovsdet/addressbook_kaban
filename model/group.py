090e72c4daa8e001aa2b402803d70439eb6e1d23
popovsdet <popovsqa@gmail.com>
2020-05-24 11:48:00 -0700

    done 4-3

--- model/group.py
+++ model/group.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Group(object):
     def __init__(self, name=None, header=None, footer=None, id=None):
         self.name = name
@@ -6,7 +9,14 @@ class Group(object):
         self.id = id
 
     def __repr__(self):
-        return f"{self.id}:{self.name}"
+        return f"id = '{self.id}': name = '{self.name}'"
 
     def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and \
+               self.name == other.name
+
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,20 +3,27 @@ from model.group import Group
 
 def test_modify_group(app, check_group):
     old_groups = app.group.get_groups()
-    app.group.modify(Group(name="name_2", header="header_2", footer="footer_2"))
+    group = Group(name="name_2", header="header_2", footer="footer_2")
+    group.id = old_groups[0].id
+    app.group.modify(group)
     new_groups = app.group.get_groups()
+
     assert len(old_groups) == len(new_groups), f"Old list '{old_groups}' != new list '{new_groups}'"
 
-
-def test_modify_group_name(app, check_group):
-    old_groups = app.group.get_groups()
-    app.group.modify(Group(name="name_2"))
-    new_groups = app.group.get_groups()
-    assert len(old_groups) == len(new_groups), f"Old list '{old_groups}' != new list '{new_groups}'"
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max), \
+        f"Old list '{old_groups}' != new list '{new_groups}'"
 
 
-def test_modify_group_header(app, check_group):
-    old_groups = app.group.get_groups()
-    app.group.modify(Group(header="header_2"))
-    new_groups = app.group.get_groups()
-    assert len(old_groups) == len(new_groups), f"Old list '{old_groups}' != new list '{new_groups}'"
+# def test_modify_group_name(app, check_group):
+#     old_groups = app.group.get_groups()
+#     app.group.modify(Group(name="name_2"))
+#     new_groups = app.group.get_groups()
+#     assert len(old_groups) == len(new_groups), f"Old list '{old_groups}' != new list '{new_groups}'"
+#
+#
+# def test_modify_group_header(app, check_group):
+#     old_groups = app.group.get_groups()
+#     app.group.modify(Group(header="header_2"))
+#     new_groups = app.group.get_groups()
+#     assert len(old_groups) == len(new_groups), f"Old list '{old_groups}' != new list '{new_groups}'"
